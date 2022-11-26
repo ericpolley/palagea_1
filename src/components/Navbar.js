0981ebf5afb1b26201d7264884bd2992c5ec6d2d
eric polley <ericpolley@live.com>
2022-11-25 19:21:27 -0800

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import {Button} from 'react-bootstrap';
+import '../polley.css'
+
+export default function Navbar() {
+  return (
+    <div className='flexRow'>
+         <Button className='button'>click me</Button>
+         <Button className='button'>click me</Button>
+         <Button className='button'>click me</Button>
+         
+     
+     </div>
+)
+}
+
+
